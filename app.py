d416315d57cc6ac2c9c6fdd5e769cb26d83ac819
svlandeg <svlandeg@github.com>
2018-12-27 20:23:40 +0000

    toying with Flask

---
+++ app.py
@@ -0,0 +1,25 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask, render_template
+import tensorflow as tf
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.route('/ml')
+def hello():
+    return 'Hello, Machine learning'
+
+@app.route('/add/<int:id1>/<int:id2>')
+def add(id1, id2):
+    value = id1+id2
+    return 'Calculated:' + str(value)
+
+@app.route('/overview/')
+def overview():
+    par = "This is some random text that doesn't have HTML formatting"
+    return render_template('overview.html', title="OxyKodit overview", body=par)
