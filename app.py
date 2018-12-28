ef19b991494392852a611d0ba6fe612aadf182e5
svlandeg <sofie.vanlandeghem@gmail.com>
2018-12-28 14:15:56 +0100

    img redirect

--- app.py
+++ app.py
@@ -3,20 +3,29 @@ import tensorflow as tf
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello world!'
 
+
 @app.route('/ml')
 def hello():
     return 'Hello, Machine learning'
+
 
 @app.route('/add/<int:id1>/<int:id2>')
 def add(id1, id2):
     value = id1+id2
     return 'Calculated:' + str(value)
 
+
 @app.route('/overview/')
 def overview():
     par = "This is some random text that doesn't have HTML formatting"
     return render_template('overview.html', title="OxyKodit overview", body=par)
+
+
+@app.route('/tufa/')
+def tufa():
+    return render_template('tufa_img.html', title="Tufa image", border_color="green")
