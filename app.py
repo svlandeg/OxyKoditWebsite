27bbab7d0aa06e36a33b25b970d738fa943ad119
svlandeg <svlandeg@github.com>
2018-12-28 18:44:46 +0000

    modulo border color

--- app.py
+++ app.py
@@ -5,12 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
-
-
-@app.route('/ml')
-def hello():
-    return 'Hello, Machine learning'
+    return 'Hello from OxyKodit  :)'
 
 
 @app.route('/add/<int:id1>/<int:id2>')
@@ -34,9 +29,25 @@ def tufa(count):
 @app.route('/grid/', methods = ['GET', 'POST'])
 def grid():
     if request.method == 'GET':
-        return render_template('tufa_grid.html', title="Tufa image grid", border_color="green")
+        border_colors="0,0,0"
+        index_clicked = -1
+
     if request.method == 'POST':
-        color = request.form.get('color')
-        return render_template('tufa_grid.html', title="Tufa image grid", border_color=color)
+        border_colors = request.form.get('border_colors')
+        index_clicked = int(request.form.get('index_clicked', -1))
 
+    color_list = border_colors.split(",")
+    if index_clicked > -1:
+        color_list[index_clicked] = str((int(color_list[index_clicked]) + 1) % 3)
+        border_colors = ",".join(color_list)
 
+    return render_template('tufa_grid.html', title="Tufa image grid", border_colors=border_colors, border_color_0=get_color(color_list[0]), border_color_1=get_color(color_list[1]), border_color_2=get_color(color_list[2]))
+
+def get_color(color_index):
+    if color_index == "0":
+        return "blue"
+    if color_index == "1":
+        return "green"
+    if color_index == "2":
+        return "red"
+    return "black"
