5fbc21e73036dd58a91a2a27c6c18287ceec075c
svlandeg <sofie.vanlandeghem@gmail.com>
2019-01-14 21:13:08 +0100

    fwd to static file while training the model

--- app.py
+++ app.py
@@ -45,12 +45,14 @@ def grid():
         preds = [0.5] * img_nr
         index_clicked = int(request.form.get('index_clicked', -1))
         to_retrain = request.form.get('retrain', 'No')
+        show_static = request.form.get('show_static', 'No')
     else:
         color_list = ["0"] * img_nr
         border_indices = ",".join(color_list)
         preds = [0.5] * img_nr
         index_clicked = -1
         to_retrain = "No"
+        show_static = "No"
 
     # if an image was clicked, change its border color and index
     color_list = border_indices.split(",")
@@ -70,19 +72,29 @@ def grid():
         train_disabled = "disabled"
 
     # train the model and make preditions
-    if allow_training and to_retrain == "Yes":
+    if allow_training and to_retrain == "Yes" and show_static == "No":
         img_size, model = get_ml_model()
         preds = ml_model.main(tufa_image_list, nontufa_image_list, all_image_list, model, img_size)
 
     # return the HTML page
-    return render_template('tufa_grid.html',
-                           grid_width=grid_width,
-                           title="Tufa image grid",
-                           border_indices=border_indices,
-                           image_indices=range(img_nr),
-                           border_colors=[_get_border_color(color_list[i]) for i in range(img_nr)],
-                           bg_colors=[_get_bg_color(preds[i]) for i in range(img_nr)],
-                           train_disabled=train_disabled)
+    if show_static == "Yes":
+        return render_template('tufa_grid_static.html',
+                               grid_width=grid_width,
+                               title="Tufa image grid",
+                               border_indices=border_indices,
+                               image_indices=range(img_nr),
+                               border_colors=[_get_border_color(color_list[i]) for i in range(img_nr)],
+                               bg_colors=[_get_bg_color(preds[i]) for i in range(img_nr)],
+                               train_disabled=train_disabled)
+    else:
+        return render_template('tufa_grid.html',
+                               grid_width=grid_width,
+                               title="Tufa image grid",
+                               border_indices=border_indices,
+                               image_indices=range(img_nr),
+                               border_colors=[_get_border_color(color_list[i]) for i in range(img_nr)],
+                               bg_colors=[_get_bg_color(preds[i]) for i in range(img_nr)],
+                               train_disabled=train_disabled)
 
 
 def _get_border_color(color_index):
@@ -101,9 +113,8 @@ def _get_bg_color(tufa_pred_float):
     tufa_pred_float = max(tufa_pred_float, 0)
     tufa_pred_float = min(tufa_pred_float, 1)
 
-    # more red if its not a tufa, more green if it's a tufa
-    r = (1-tufa_pred_float)*255
+    # more red if it's not a tufa, more green if it's a tufa
+    r = (1 - tufa_pred_float) * 255
     g = tufa_pred_float * 255
 
     return "(" + str(int(r)) + "," + str(int(g)) + ",127, " + str(transparant) + ")"
-
